c08a7ab7ba2d711807080debb9b23f2cd5559efc
victoria.set.1999 <victoria.set.1999@gmail.com>
2021-11-23 13:21:01 -0300

    adequacao a formatacao camelCase

--- Laboratório1/lab1.c
+++ Laboratório1/lab1.c
@@ -10,29 +10,29 @@ principal), para elevar ao quadrado cada elemento de um vetor de 10000 elementos
 
 #define NTHREADS  2 //total de threads a serem criadas
 #define NELEMENTS 1000 //total de elementos no vetor
-#define max_number 10 //máximo elemento possível do vetor
-#define min_number 0 //mínimo elemento possível do vetor
+#define MAX_RND 10 //máximo elemento possível do vetor
+#define MIN_RND 0 //mínimo elemento possível do vetor
 
-int pwrvector[NELEMENTS]; //vetor que será modificado, inicialmente identico ao original
+int g_pwrVector[NELEMENTS]; //vetor que será modificado, inicialmente identico ao original
 
 //cria a estrutura de dados para armazenar os argumentos da thread
 typedef struct {
    int idThread, limit;
-} t_Args;
+} TArgs;
 
 
 //funcao para inicializar o vetor de forma aleatória com inteiro, nesse caso, de 0 até 10
-void initVector(int* vector) {
+void init_vector(int* vector) {
     srand(time(0));
     for(int i = 0; i < NELEMENTS; i++) {
-        vector[i] = rand() % (max_number + 1 - min_number) + min_number;
-        pwrvector[i] = vector[i]; //copia cada elemento do vetor original no vetor que modificaremos
+        vector[i] = rand() % (MAX_RND + 1 - MIN_RND) + MIN_RND;
+        g_pwrVector[i] = vector[i]; //copia cada elemento do vetor original no vetor que modificaremos
     }
 }
 
 //funcao executada pelas threads que efetua a elevação ao quadrado dos elementos do vetor
-void *Pwr2 (void* arg) {
-    t_Args *args = (t_Args *) arg;
+void *pwr2 (void* arg) {
+    TArgs *args = (TArgs *) arg;
 
     int start = args->idThread*args->limit; //calcula a posição de inicio da operação, dependendo de qual thread esta sendo executada
 
@@ -40,13 +40,13 @@ void *Pwr2 (void* arg) {
     //(para evitar erros com divisões com resto)
     if (args->idThread+1 == NTHREADS){
 	    for (int i = start; i<NELEMENTS; i++){
-            pwrvector[i] = pwrvector[i] * pwrvector[i];
+            g_pwrVector[i] = g_pwrVector[i] * g_pwrVector[i];
         }
     }
     else{
         //caso não esteja na última thread faz as operações nos elementos até atingir a quantidade calculada anteriormente (divisions)
         for (int i = start; i <start+args->limit; i++){
-		    pwrvector[i] = pwrvector[i] * pwrvector[i];
+		    g_pwrVector[i] = g_pwrVector[i] * g_pwrVector[i];
 	    }
     }
     pthread_exit(NULL);                                                                       
@@ -54,34 +54,34 @@ void *Pwr2 (void* arg) {
 
 //funcao principal do programa
 int main() {
-    pthread_t tid_sistema[NTHREADS]; //identificadores das threads no sistema
+    pthread_t tidSistema[NTHREADS]; //identificadores das threads no sistema
     int thread; //variavel auxiliar
-    int tid_local[NTHREADS]; //identificadores locais das threads
+    int tidLocal[NTHREADS]; //identificadores locais das threads
     int divisions = (NELEMENTS/NTHREADS)-1; //quantos elementos cada thread operará em
-    t_Args *arg; //receberá os argumentos para a thread
+    TArgs *arg; //receberá os argumentos para a thread
     int vector[NELEMENTS]; //vetor original
     
-    initVector(vector); //inicializa o vetor
+    init_vector(vector); //inicializa o vetor
 
     //cria as  threads
     for(thread=0; thread<NTHREADS; thread++) {
-        tid_local[thread] = thread;
+        tidLocal[thread] = thread;
 
-        arg = malloc(sizeof(t_Args));
+        arg = malloc(sizeof(TArgs));
         if (arg == NULL) {
             printf("--ERRO: malloc()\n"); exit(-1);
         }
         arg->idThread = thread; 
         arg->limit = divisions; 
 
-        if (pthread_create(&tid_sistema[thread], NULL, Pwr2,  (void*) (void*) arg)) {
+        if (pthread_create(&tidSistema[thread], NULL, pwr2,  (void*) arg)) {
         printf("--ERRO: pthread_create()\n"); exit(-1);
         }
     }
 
     //espera todas as threads terminarem
     for (thread=0; thread<NTHREADS; thread++) {
-        if (pthread_join(tid_sistema[thread], NULL)) {
+        if (pthread_join(tidSistema[thread], NULL)) {
             printf("--ERRO: pthread_join() \n"); exit(-1); 
         } 
     }
@@ -92,7 +92,7 @@ int main() {
     //itera pelo vetor comparando cada elemento do vetor original elevado ao quadrado com o vetor modificado
     //caso algum não seja igual muda a variavel
     for(int i = 0; i < NELEMENTS; i++) {
-        if(vector[i] * vector[i] != pwrvector[i]) {
+        if(vector[i] * vector[i] != g_pwrVector[i]) {
             isCorrect = 0;
             break;
         }
