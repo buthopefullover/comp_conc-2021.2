b6775d5ce6dbaf5be6f97d244a34c5a434ad8718
victoria.set.1999 <victoria.set.1999@gmail.com>
2022-02-01 23:43:03 -0300

    comentarios e melhor organizacao de arquivos

--- OrderBook/OrderBook.c
+++ OrderBook/OrderBook.c
@@ -3,153 +3,180 @@
 #include<string.h>
 #include<pthread.h>
 #include <ctype.h>
-#include <locale.h>
+#include "timer.h"
+#include "CheckAlloc.c"
+#include "Merge.c"
+#include "Palavra.h"
 
-typedef struct{
-    char *letras;
-}Palavra;
 
-Palavra *Livro;
-int TamanhoPalavras=100;
+Palavra *Livro; // Variável global que armazena todas as palavras do livro
 
-//variaveis de condicao das threads
+// variaveis de condicao das threads
 pthread_mutex_t x_mutex;
 pthread_cond_t x_cond;
-int liberaThread = 0;//validador 0 ou 1
-int contaThread=0;
-int bloqueadas = 0;
 
-//acentos a ser incluidos
-char acentoA[] = {"áÁãÃàÀâÂ"};
-char acentoE[] = {"éÉèÈêÊ"};
-char acentoI[] = {"íÍìÌ"};
-char acentoO[] = {"óÓòÒôÔõÕ"};
-char acentoU[] = {"úÚùÙûÛ"};
-char acentoC[] = {"çÇ"};
-
-//numStucts alocadas
+// quantidade de structs (Palavra) alocadas
 long int numStructs=10;
-//numStructs utilizadas
+// quantidade de structs (Palavra) armazenadas
 long int storedStructs=0;
 
-//num threads
-int nthreads =3;
+// número de threads
+int nthreads =9;
 
+// contador de broadcasts emitidos
 int nBroad = 0;
 
 
-// funcao para tratar erros com o malloc
-void *errorcheck_realloc(int SIZEARR, void *ptr) {
-    void* temp = realloc(ptr, SIZEARR);
-    if(temp == NULL) {
-        printf("--ERRO: realloc()\n");
-        exit(-1);
-    }
-    return temp;
-    
-}
-
-// funcao para tratar erros com o malloc
-void *errorcheck_malloc(int SIZEARR) {
-    void *ptr = malloc(SIZEARR);
-    if(ptr == NULL) {
-        printf("--ERRO: malloc()\n");
-        exit(-1);
-    }
-    return ptr;
-}
-
+/**
+ * Função que percorre o arquivo e faz uma estimativa
+ * para cima de quantas palavras possuem.
+ * 
+ * @param file arquivo de texto que será contado as palavras
+ * @return estimativa de número de palavras
+ **/
 long int wordCounter(FILE * file){
     long int spaceCounter = 0;
     char ch;
 
-    while ((ch = fgetc(file)) != EOF) 
+    while ((ch = fgetc(file)) != EOF) // percorre o arquivo todo
     {
-        if (isspace(ch))
+        if (isspace(ch)) // caso o caracter seja um espaço incrementa o contador
             ++spaceCounter;
     }
     return spaceCounter;
 }
 
-int isAcento(const char *string, char character)
+
+/**
+ * Função que le um arquivo sequencialmente parseando as palvras que acha
+ * e armazenando na struct global Livro.
+ * 
+ * @param arg nome do arquivo a ser lido
+ **/
+void readFileSeq(char * arg)
 {
-    const char *pointer;
-
-    pointer = strchr(string, character);
-
-    if(pointer)
-        return 1;
-    else
-        return 0;
-}
-
-int isAcento2(char ch, char lb){
-    if(lb == -61){
-        if(isAcento(acentoA, ch)){
-            return 1;
-        }
-        else if(isAcento(acentoE, ch)){
-            return 1;
-        }
-        else if(isAcento(acentoI,ch)){
-            return 1;
-        }
-        else if(isAcento(acentoO,ch)){
-            return 1;
-        }
-        else if(isAcento(acentoU,ch)){
-            return 1;
-        }
-        else if(isAcento(acentoC,ch)){
-            return 1;
-        }
-    }
-    return 0;
-}
-
-
-// le o aquivo pegando as palavras e armazenando-as no vetor de struct global (Palavras)
-void* readFile(void * arg)
-{
-    char* fileName = (char*) arg;
+    char* fileName = arg;
     FILE *file;
-    char lookBehind;
-    char ch; 
-    long int razao;
-    long int limit;
+    char lookBehind; // variavel que armazena o caracter lido anteriormente
+    char ch; // variavel que armazena o caracter lido atualmente
 
-    int l = 0;
-    int p = 0;
+    int l = 0; // contador que acompanha a letra lida 
+    int p = 0; // contador que acompanha a palavra lida 
 
     file = fopen(fileName, "r");
-    if (file == NULL){
+    if (file == NULL){ // checa se houve falha na abertura do arquivo
         printf("Falha ao abrir o arquivo!");
-        exit(-1); // não consegui abrir o arquivo
+        exit(-1); 
     }
 
-    
+    // calcula e armazena uma estima das palavras que existem no arquivo
     numStructs = wordCounter(file);
-    razao = numStructs/(nthreads-1);
-    limit = razao;
 
+    // volta a leitura para o inicio do arquivo pois a chamada 
+    //do wordCounter mexeu no pontero de leitura
     rewind(file);
 
-
+    // aloca espaço para todas as palavras baseando-se na estimativa calculada
     Livro = (Palavra*) errorcheck_malloc(sizeof(Palavra)*numStructs);
     for (int i = 0; i < numStructs; i++){
-        Livro[i].letras = (char*) errorcheck_malloc(sizeof(char)*TamanhoPalavras);
+        Livro[i].letras = (char*) errorcheck_malloc(sizeof(char)*TAMANHOPALAVRAS);
     }
 
+    // percorre o arquivo
     while ((ch = fgetc(file)) != EOF)
     {
-
+        // se for um caracter alfanumerico incluimos na palavra que estamos armazenando
         if(isalnum(ch)){
             Livro[p].letras[l] = ch;
+            l++; // incrementa o contador de letras para irmos para a próxima posição
+        }
+
+        // checa se o caracter que estamos lendo é um acento, checando o anterior e o atual
+        // pois um acento é composto de dois caracteres. depois armazena ambos os caracteres
+        // na palavra
+        if(isAcento(ch, lookBehind)){
+            Livro[p].letras[l] = lookBehind; 
             l++;
+            Livro[p].letras[l] = ch;
+            l++;
+            
+        }
+
+        // checa se a palavra que estava sendo lida foi terminada, checando primeiro se o caracter 
+        // atual é um breakpoint (espaço, hífen, quebra de linha, pontuação) e depois checando se o anterior
+        // é um acento ou um caracter alfanumérico
+        if((isspace(ch)||ch == '-'||ispunct(ch)) && (isalnum(lookBehind)||isAcento(Livro[p].letras[l-1], Livro[p].letras[l-2]))){
+            p++; // incrementa o contador de palavras para irmos para a próxima posição
+            l = 0; // zera o contador já que esta palavra foi terminada
+        }
+
+        // armazena o caracter que vai ser o anterior na próxima iteração
+        lookBehind = ch;
+    }
+
+    storedStructs = p; // armazena a quantidade de palavras lidas na variavel global de palavras armazenadas
+
+    fclose(file);  // fecha o arquivo
+
+}
+
+/**
+ * Função que le um arquivo concorrentemente parseando as palvras que acha
+ * e armazenando na struct global Livro.
+ * 
+ * @param arg nome do arquivo a ser lido
+ **/
+void* readFileConc(void * arg)
+{
+    char* fileName = (char*) arg; 
+    FILE *file;
+    char lookBehind; // variavel que armazena o caracter lido anteriormente
+    char ch; // variavel que armazena o caracter lido atualmente
+    long int razao; // armazena a razao usada na divisão da threads de ordenação
+    long int limit; // armazena o limite da nova thread que será liberada
+
+    int l = 0; // contador que acompanha a letra lida 
+    int p = 0; // contador que acompanha a palavra lida 
+
+    file = fopen(fileName, "r");
+    if (file == NULL){ // falha ao abrir o arquivo
+        printf("Falha ao abrir o arquivo!");
+        exit(-1);
+    }
+
+    // calcula e armazena uma estima das palavras que existem no arquivo
+    numStructs = wordCounter(file);
+
+    // calcula a razao usada na divisão da threads de ordenação
+    razao = numStructs/(nthreads-1);
+
+    //inicia o limite da primeira thread que será liberada
+    limit = razao;
+
+    // volta a leitura para o inicio do arquivo pois a chamada 
+    //do wordCounter mexeu no pontero de leitura
+    rewind(file);
+
+    // aloca espaço para todas as palavras baseando-se na estimativa calculada
+    Livro = (Palavra*) errorcheck_malloc(sizeof(Palavra)*numStructs);
+    for (int i = 0; i < numStructs; i++){
+        Livro[i].letras = (char*) errorcheck_malloc(sizeof(char)*TAMANHOPALAVRAS);
+    }
+
+    // percorre o arquivo
+    while ((ch = fgetc(file)) != EOF)
+    {
+        // se for um caracter alfanumerico incluimos na palavra que estamos armazenando
+        if(isalnum(ch)){
+            Livro[p].letras[l] = ch;
+            l++; // incrementa o contador de letras para irmos para a próxima posição
 
         }
 
-        if(isAcento2(ch, lookBehind)){
+        // checa se o caracter que estamos lendo é um acento, checando o anterior e o atual
+        // pois um acento é composto de dois caracteres. depois armazena ambos os caracteres
+        // na palavra
+        if(isAcento(ch, lookBehind)){
             Livro[p].letras[l] = lookBehind;
             l++;
             Livro[p].letras[l] = ch;
@@ -157,43 +184,53 @@ void* readFile(void * arg)
             
         }
 
-        if((isspace(ch)||ch == '-'||ispunct(ch)) && (isalnum(lookBehind)||isAcento2(Livro[p].letras[l-1], Livro[p].letras[l-2]))){
-            p++;
-            storedStructs++; // posso só igualar com p antes de finalizar
-            
-            l = 0;
+        // checa se a palavra que estava sendo lida foi terminada, checando primeiro se o caracter 
+        // atual é um breakpoint (espaço, hífen, quebra de linha, pontuação) e depois checando se o anterior
+        // é um acento ou um caracter alfanumérico
+        if((isspace(ch)||ch == '-'||ispunct(ch)) && (isalnum(lookBehind)||isAcento(Livro[p].letras[l-1], Livro[p].letras[l-2]))){
+            p++; // incrementa o contador de palavras para irmos para a próxima posição
+            storedStructs++; // incrementa as palavras que foram armazenadas
+            l = 0; // zera o contador já que esta palavra foi terminada
 
+            // se a palavra é a última do limite que a thread irá ordenar libera ela
             if (p == limit){
-                pthread_mutex_lock(&x_mutex);
-                nBroad++;
-                
+                // bloqueia as threads de executarem pois iremos alterar uma váriavel global que elas utilizam
+                pthread_mutex_lock(&x_mutex); 
+                nBroad++; // incrementa o contador de broadcast para não se perder           
                 pthread_mutex_unlock(&x_mutex);
-                pthread_cond_broadcast(&x_cond);
+
+                pthread_cond_broadcast(&x_cond); // libera as threads
                 
                 
-                limit += razao;
+                limit += razao; // calcula o limite da próxima thread a ser liberada
                 
             }
-
         }
         lookBehind = ch;
     }
-    if (ch == EOF){
-        pthread_mutex_lock(&x_mutex);
-        nBroad++;
+    // libera a ultima thread de ordenação pois terminamos de ler todas as palavras
+    pthread_mutex_lock(&x_mutex);
+    nBroad++;
+    pthread_mutex_unlock(&x_mutex);
 
-        pthread_mutex_unlock(&x_mutex);
+    pthread_cond_broadcast(&x_cond);                
 
-        pthread_cond_broadcast(&x_cond);                
-    }
         
     fclose(file);  
+
+    printf("Quantidade de palavras: %ld\n", storedStructs);
 
     pthread_exit(NULL);
 
 
 }
 
+/**
+ * Função que escreve cada palavra da variável global Livro
+ * em um arquivo
+ * 
+ * @param fileName nome do arquivo que será escrito
+ **/
 void writeFile (char *fileName){
     FILE *file = fopen(fileName, "w");
 
@@ -211,184 +248,20 @@ void writeFile (char *fileName){
 
 }
 
-int substituiAcento(char a){
-    if(isAcento(acentoA, a)){
-        return 97;//retorna o codigo de a
-    
-    }        
-    else if(isAcento(acentoE, a)){
-        return 101;//retorna o codigo de e
-    
-    }
-    else if(isAcento(acentoI,a)){
-        return 105;//retorna o codigo de i
-    
-    }
-    else if(isAcento(acentoO,a)){
-        return 111;//retorna o codigo de o
-    
-    }
-    else if(isAcento(acentoU,a)){
-        return 117;//retorna o codigo de u
-    
-    }
-    else if(isAcento(acentoC,a)){
-        return 99;//retorna o codigo de c
-        
-    }
-    return 0;
-}
-
-/**
- * 1 - palavra2 antes palavra1
- * 0 - iguais
- * -1 - palavra1 antes palavra2 
- */ 
-int stringcomp(char *palavra1, char *palavra2){
-    char* temp1 = (char*) errorcheck_malloc(sizeof(char)*strlen(palavra1));
-    char* temp2 = (char*) errorcheck_malloc(sizeof(char)*strlen(palavra2));
-
-    strcpy(temp1, palavra1);
-    strcpy(temp2, palavra2);
-    
-    int achouAcentoI =0;
-    int achouAcentoK =0;
-    int achouAcento =0;
-
-    for(int i =0, k=0; i<strlen(palavra1) && k<strlen(palavra2); i++, k++){
-        achouAcentoI =0;
-        achouAcentoK =0;
-        achouAcento =0;
-        
-        if(temp1[i]==-61||temp2[i]==-61){ // tratamos acento na palavra 1
-            
-            if(temp1[i]==-61){
-                temp1[i] = substituiAcento(temp1[i+1]);
-                i++;
-                achouAcentoI++;
-                achouAcento++;
-            }
-            if(temp2[k]==-61){
-                temp2[k] = substituiAcento(temp2[k+1]);
-                k++;
-                achouAcentoK++;
-                achouAcento++;
-            }
-            
-            if(achouAcentoI){
-                if(achouAcentoK){
-                    if(tolower(temp1[i-1])>tolower(temp2[k-1]))
-                        return 1;
-                    else if(tolower(temp1[i-1])<tolower(temp2[k-1]))
-                        return -1;
-                }
-                else{
-                    if(tolower(temp1[i-1])>tolower(temp2[k]))
-                        return 1;
-                    else if(tolower(temp1[i-1])<tolower(temp2[k]))
-                        return -1;
-                }
-            }
-            if(achouAcentoK && !achouAcentoI){
-                if(tolower(temp1[i])>tolower(temp2[k-1]))
-                        return 1;
-                else if(tolower(temp1[i])<tolower(temp2[k-1]))
-                    return -1;
-            }
-
-            if(!achouAcento){
-                if(tolower(temp1[i])>tolower(temp2[k]))
-                    return 1;
-                else if(tolower(temp1[i])<tolower(temp2[k]))
-                    return-1;
-            }
-            continue;
-        }
-        
-        if ( tolower(temp1[i]) > tolower(temp2[k]) )
-            return 1;
-        
-        else if( tolower(temp1[i]) < tolower(temp2[k]) )
-            return -1;
-        
-            
-    }
-
-    if((strlen(palavra1)-achouAcentoI)>(strlen(palavra2)-achouAcentoK))
-        return 1;
-    else if((strlen(palavra1)-achouAcentoI)<(strlen(palavra2)-achouAcentoK))
-        return -1; 
-    
-    return strcmp(palavra1, palavra2);
-    
-    
-}
 
 
 
-
-
-void Merge(Palavra *Lista, int inicio, int meio, int fim)//passar tamanho da struct no fim
-{
-    Palavra *listaTemporaria; int aux1, aux2, fimAux1, fimAux2, tamanho;  
-    tamanho = fim - inicio +1; // admitindo que o inicio pode ser o indice 0 somamos 1;
-    aux1 = inicio; aux2 = meio+1; //aux1: indice de inicio da primeira divisao //aux2: indice de inicio da segunda divisao
-    fimAux1 = 0; fimAux2 = 0; //validadores de fim das partes divididas
-    listaTemporaria = (Palavra *) errorcheck_malloc(tamanho*sizeof(Palavra)); //Lista temporaria com o tamanho do vetor a ser ordenado
-    for (int i = 0; i < tamanho; i++){
-        listaTemporaria[i].letras = (char*) errorcheck_malloc(sizeof(char)*TamanhoPalavras);
-    }
-
-    for(int i=0; i<tamanho; i++)
-    {
-        if(!fimAux1 && !fimAux2)
-        {
-            if(stringcomp(Lista[aux1].letras, Lista[aux2].letras)<0)
-                listaTemporaria[i] = Lista[aux1++];
-            else 
-                listaTemporaria[i]= Lista[aux2++];
-            if(aux1 > meio)
-                fimAux1 = 1;
-            if(aux2 > fim)
-                fimAux2 = 1;
-        }
-        else 
-        {
-            if (!fimAux1)
-                listaTemporaria[i] = Lista[aux1++];
-            else 
-                listaTemporaria[i] = Lista[aux2++];
-        }
-    }
-    for(int j=0 , k = inicio; j<tamanho; j++, k++)
-        Lista[k]= listaTemporaria[j];
-    
-    free(listaTemporaria);
-    return;
-}
-
-
-
-
-void MergeSort(Palavra *Lista, int inicio, int fim)
-{
-    int meio = (fim+inicio)/2;
-    if(inicio < fim){ 
-        MergeSort(Lista, inicio, meio);
-        MergeSort(Lista, meio+1, fim); 
-        Merge(Lista, inicio, meio, fim); 
-    }
-    return;
-}
 
 
 
 void * executaThread(void * arg){
-    int ident = *(int *)arg;
+    int ident = *(int *)arg; // id da thread
+    
+    // bloqueia a as outras thread para que ela ou entre no wait
+    // para esperar o broadcast ou checar se os broadcast que já mandaram
+    // incluem ela
     pthread_mutex_lock(&x_mutex);
-
     while(ident > nBroad){ 
-
         pthread_cond_wait(&x_cond, &x_mutex);
     }
     
@@ -396,6 +269,7 @@ void * executaThread(void * arg){
     
     int razao = numStructs/(nthreads-1);
     int limite = ident * razao;
+
     if(ident == nthreads-1){
         MergeSort(Livro, (ident-1)* razao, numStructs-1);
     }
@@ -412,6 +286,7 @@ void * executaThread(void * arg){
 
 
 int main(int argc, char *argv[]) {
+    double start, finish, tempSeq, tempConc;
     
     nthreads++;
 
@@ -424,38 +299,71 @@ int main(int argc, char *argv[]) {
     pthread_cond_init(&x_cond, NULL);
 
 
+    // Executando concorrentemente
+    GET_TIME(start);
     for(int i=0; i<nthreads;i++){
-        ident[i]=i;//ident[i]=i+1;
-        char* fileName = "Quem e voce, Alasca_ - John Green (2).txt";
+        ident[i]=i;
+        char* fileName = "O-Cortiço-Aluísio-de-Azevedo.txt";
+        // caso seja a primeira thread, executa a leitura do arquivo
         if(i == 0){ 
-            pthread_create(&tid[i], NULL, readFile, (void *)fileName);
+            pthread_create(&tid[i], NULL, readFileConc, (void *)fileName);
         }
         else{
             pthread_create(&tid[i], NULL, executaThread, (void *)&ident[i]);
         }
     }
 
-    //aguarda retornoi das threads
+    //aguarda retorno das threads
     for(int i=0; i<nthreads;i++){
       if(pthread_join(tid[i], NULL))
         printf("Erro: pthread create\n");
         
     }
 
+    // executa a última etapa do merge, juntando todas as partes do vetor 
+    // que já foram ordenadas
     for(int i=1; i<nthreads-1; i++){  
         int razao = numStructs/(nthreads-1);
-        int limite = i * razao; //id da thread * razao
-        //int meio = (fim+inicio)/2;
+        int limite = i * razao; 
     
         if(i == nthreads-2){
             Merge(Livro, 0, limite -1, numStructs-1);
         }
         else{
-            Merge(Livro, 0, limite -1, ((i+1)*razao)-1);//alterar fim e meio
+            Merge(Livro, 0, limite -1, ((i+1)*razao)-1);
         }
     } 
-    writeFile("sorted.txt");
 
+    GET_TIME(finish);
+
+    tempConc = finish - start;
+
+    writeFile("sortedConc.txt");
+
+    // libera o espaço do vetor para a execução sequencial
+    free(Livro);
+
+    // reseta o valor de palavras para a execução sequencial
+    storedStructs = 0;
+
+    // Agora executando sequencialmente
+
+    GET_TIME(start);
+    char* fileName = "O-Cortiço-Aluísio-de-Azevedo.txt";
+
+    readFileSeq(fileName);
+
+    MergeSort(Livro, 0, numStructs-1);
+    
+    GET_TIME(finish);
+
+    tempSeq = finish - start;
+
+    writeFile("sortedSeq.txt");
+
+    printf("1 thread de leitura e %d threads de ordenação\n", nthreads-1);
+    printf("Tempo de execução concorrente: %f\n", tempConc);
+    printf("Tempo de execução sequencial: %f\n", tempSeq);
 
     return 0;
 
--- OrderBook/teste.c
+++
@@ -1,9 +0,0 @@
-#include <locale.h>
-#include <stdio.h>
-
-int main (void) {
-   char buffer[100];
-   scanf ("%s", buffer); 
-   printf ("%s\n", buffer);
-   return 0;
-}
